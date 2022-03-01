73bb83e026857e2f275edfe5785710c64e5c69a6
Vilmarys R <43301644+vilrodriguez@users.noreply.github.com>
2022-03-01 12:47:06 -0500

    primer commmit de js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { example } from './data.js';
+import { getPokemons } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+//console.log(filterData, data);
 
-console.log(example, data);
+const pokemons = document.getElementById('pokemons');
+
+// DATA VARIABLES
+const showPokemons = getPokemons(data.pokemon);
+
+console.log(showPokemons)
