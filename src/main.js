18cb3ab6962817dd15679f503bedb55cc17862d1
Vilmarys R <43301644+vilrodriguez@users.noreply.github.com>
2022-03-02 10:23:09 -0500

    updating main

--- src/main.js
+++ src/main.js
@@ -4,10 +4,8 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 //console.log(filterData, data);
 const pokemonsMainDiv = document.getElementById('main__div-Characteres');
-
 // DATA VARIABLES funcion -- // pokemonNew
 const pokeData = getPokemons(data.pokemon);
-
 // funcion mostrar pokemones
 const showPokemonsDiv = (pokeData) => {
   let showAll = "";
@@ -16,5 +14,18 @@ const showPokemonsDiv = (pokeData) => {
     let showEachOne =
       `<div id="pokemonCard" class="card">
         <div class="cardImg">
+          <img class="main__div-P-Img"src= "${pokeData[i].image}" />
+        </div>
+        <div class="cardInfo">
+          <p class="main__div-P-Numero">${pokeData[i].number}</p>
+          <p class="main__div-P-Nombre">${pokeData[i].name}</p>
+          <button class="main__div-P-Boton">Ver información</button>
+        </div>
+      </div>`;
+    showAll += showEachOne;
+  }
+  return showAll;
 }
+pokemonsMainDiv.innerHTML = showPokemonsDiv(pokeData);
 // imprime la data de los pokemones
+//console.log(pokeData)
